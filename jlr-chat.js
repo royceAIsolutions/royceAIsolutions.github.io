2e2a465af219f0ed92a1b4de1c4f480ec232ac43
royceAI <royceaisolutions@gmail.com>
2026-09-10 13:12:58 -0700

    JLR chat: repoint widget to live tunnel https://advisory-strong-planes-display.trycloudflare.com

--- jlr-chat.js
+++ jlr-chat.js
@@ -2,7 +2,7 @@
    PIN-gated (SHA-256 client-side), talks to the bridge + Hermes webhook. */
 (function () {
   'use strict';
-  var API = 'https://love-textbooks-massive-both.trycloudflare.com';
+  var API = 'https://advisory-strong-planes-display.trycloudflare.com';
   var AUTH_KEY = 'jlr_chat_pin_hash';
   var HIST_KEY = 'jlr_chat_history';
   var AUTH_TTL = 30 * 24 * 3600 * 1000; // 30 days
